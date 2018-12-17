5e39700ef2f905a6f705dd4fc4f0995ebcc496b3
Mauricio Leal <mauricio.leal@s5stratos.com>
2018-12-17 09:55:10 -0600

    Updated App styles.

---
+++ src/App.styles.js
@@ -0,0 +1,10 @@
+import { style } from 'typestyle';
+
+export default {
+  root: style({
+    width: 450,
+    height: 510,
+    boxSizing: 'border-box',
+    padding: '48px 40px 36px 40px'
+  }),
+};

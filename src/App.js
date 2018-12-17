f27aa5e6d83499ce0cd81138d6c50fa8456a2464
Mauricio Leal <mauricio.leal@s5stratos.com>
2018-12-17 09:51:49 -0600

    Updated App.js with UpdateContacts, isLoggedIn, logOut, and Sing/Create.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
+import cloneDeep from 'lodash/cloneDeep';
+
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import CardContent from '@material-ui/core/CardContent';
 
 import CreateContact from './components/CreateContact';
 import Login from './components/Login';
-import logo from './logo.svg';
 import Register from './components/Register';
 import Header from './components/Header/Header';
+import Contacts from './components/Contacts/Contacts';
 import './App.css';
 
+import styles from './App.styles';
+
+const initialState = {
+  userContacts: [],
+  userId: '',
+  userJwtToken: '',
+  signIn: true,
+};
 export default class App extends Component {
   constructor() {
     super();
-    this.state = {
-      userJwtToken: '',
-      userId: '',
-    };
+    this.state = cloneDeep(initialState);
     this.updateToken = this.updateToken.bind(this);
     this.updateUserId = this.updateUserId.bind(this);
+    this.updateContacts = this.updateContacts.bind(this);
+    this.createSignInToggle = this.createSignInToggle.bind(this);
+    this.logOut = this.logOut.bind(this);
   }
 
   updateToken(jwtToken) {
@@ -30,15 +43,66 @@ export default class App extends Component {
     });
   }
 
+  updateContacts(userContacts) {
+    this.setState({
+      userContacts,
+    });
+  }
+
+  isLoggedIn() {
+    const { userJwtToken, userId } = this.state;
+    return userJwtToken && userId;
+  }
+
+  logOut() {
+    this.setState(cloneDeep(initialState));
+  }
+
+  createSignInToggle() {
+    this.setState(prevState => ({ signIn: !prevState.signIn }));
+  }
+
   render() {
+    const { userJwtToken, userId, userContacts, signIn } = this.state;
+    const hasContacts = userContacts.length > 0;
+    const isLoggedIn = this.isLoggedIn();
     return (
       <div className="App">
-        <Header userId={this.state.userId} />
+        <Header userId={userId} isLoggedIn={this.isLoggedIn()} logOut={this.logOut} />
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Register />
-          <CreateContact userJwtToken={this.state.userJwtToken} userId={this.state.userId} />
-          <Login updateToken={this.updateToken} updateUserId={this.updateUserId} />
+          {isLoggedIn ? (
+            <CreateContact userJwtToken={userJwtToken} userId={userId} updateContacts={this.updateContacts} />
+          ) : (
+            <Paper classes={{ root: styles.root }}>
+              {signIn ? (
+                <React.Fragment>
+                  <h1>Sign In</h1>
+                  <Login
+                    updateToken={this.updateToken}
+                    updateUserId={this.updateUserId}
+                    updateContacts={this.updateContacts}
+                  />
+                  <CardContent>
+                    <Button onClick={this.createSignInToggle} color="primary">
+                      Create account
+                    </Button>
+                  </CardContent>
+                </React.Fragment>
+              ) : (
+                <React.Fragment>
+                  <h1>Create Account</h1>
+                  <Register />
+                  <CardContent>
+                    <Button onClick={this.createSignInToggle} color="primary">
+                      Sign In
+                    </Button>
+                  </CardContent>
+                </React.Fragment>
+              )}
+            </Paper>
+          )}
+          <hr />
+          {hasContacts && <Contacts contacts={userContacts} />}
         </div>
       </div>
     );

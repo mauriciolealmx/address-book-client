c1f1f52bd0ce6744d1d8dced12e20f45ae4a8632
Mauricio Leal <mauricio.leal@s5stratos.com>
2018-12-17 09:54:55 -0600

    Changed to a simpler Header.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,77 +1,26 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
+import Button from '@material-ui/core/Button';
 import { styles } from "./Header.styles";
 
-class MenuAppBar extends React.Component {
-  state = {
-    auth: true,
-    anchorEl: null,
-  };
-
-  handleChange = event => {
-    this.setState({ auth: event.target.checked });
-  };
-
-  handleMenu = event => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
-
-  handleClose = () => {
-    this.setState({ anchorEl: null });
-  };
-
-  render() {
-    const { classes, userId } = this.props;
-    const { anchorEl } = this.state;
-    const open = Boolean(anchorEl);
-
-    return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-              =
-            </IconButton>
-            <Typography variant="h6" color="inherit" className={classes.grow}>
-            </Typography>
-            <div>
-              <IconButton
-                aria-owns={open ? 'menu-appbar' : undefined}
-                aria-haspopup="true"
-                onClick={this.handleMenu}
-                color="inherit"
-              >
-                {userId}
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={open}
-                onClose={this.handleClose}
-              >
-                <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-                <MenuItem onClick={this.handleClose}>My account</MenuItem>
-              </Menu>
-            </div>
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
-  }
+function ButtonAppBar(props) {
+  const { userId } = props;
+  return (
+    <div style={styles.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Button color="inherit">=</Button>
+          <Typography variant="h6" color="inherit" style={styles.grow}>
+            Welcome!
+          </Typography>
+          {props.isLoggedIn && <Button onClick={props.logOut} color="inherit">{'Logout'}</Button>}
+          <Button color="inherit">{userId || 'LOGIN'}</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
-export default withStyles(styles)(MenuAppBar);
+export default ButtonAppBar;

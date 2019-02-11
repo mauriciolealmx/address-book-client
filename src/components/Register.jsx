2b015f213e53303772ace2a3b8f2591a13840eeb
Mauricio Leal <mauricio.leal@s5stratos.com>
2019-02-11 16:27:35 -0600

    Use of ActionForm in Register.jsx plus the use of async await.

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-
+import ActionForm from './ActionFrom';
 import { createAccount } from '../services';
 
 const initialState = {
-  email: '',
-  password: '',
   serverError: '',
   successMessage: '',
 };
@@ -17,63 +12,37 @@ export default class Register extends Component {
   constructor(props) {
     super(props);
     this.state = initialState;
-
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(name, event) {
+  handleSubmit = async user => {
+    const { email } = await createAccount(user);
     this.setState({
-      [name]: event.target.value,
+      ...initialState,
+      successMessage: `Registered user ${email}`,
     });
-  }
-
-  handleSubmit() {
-    const { email, password } = this.state;
-    const user = { email, password };
-
-    createAccount(user)
-      .then(user => {
-        const { email } = user;
-        this.setState({
-          ...initialState,
-          successMessage: `Registered user ${email}`,
-        });
-      })
-      .catch(err => {
-        const { data } = err.response;
-        this.setState({
-          serverError: data.error,
-          successMessage: '',
-        });
-      });
-  }
+    // TODO: Need to take error into account.
+    // .catch(err => {
+    //   const { data } = err.response;
+    //   this.setState({
+    //     serverError: data.error,
+    //     successMessage: '',
+    //   });
+    // });
+  };
 
   render() {
     const { serverError, successMessage } = this.state;
+    const { alternativeButtonAction } = this.props;
 
     return (
       <div style={{ display: 'inline-block' }}>
-        <form noValidate autoComplete="off">
-          <TextField
-            autoComplete="email"
-            fullWidth
-            label="Email"
-            margin="normal"
-            onChange={event => this.handleChange('email', event)}
-            value={this.state.email}
-          />
-          <TextField
-            fullWidth
-            label="Password"
-            margin="normal"
-            onChange={event => this.handleChange('password', event)}
-            type="password"
-            value={this.state.password}
-          />
-          <Button color="primary" onClick={this.handleSubmit} variant="outlined" fullWidth>
-            Submit
-          </Button>
-        </form>
+        <ActionForm
+          cardTitle="Create account"
+          submitButtonText="Submit"
+          onSubmit={this.handleSubmit2}
+          alternativeButtonText="Sign In"
+          alternativeButtonAction={alternativeButtonAction}
+        />
         {serverError && <div>{serverError}</div>}
         {successMessage && (
           <div>
